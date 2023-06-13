99debef8a3efc71837cd196c895c1510dbe6723a
Carlos Eduardo Martínez Aguilar <cmartineza@ciencias.unam.mx>
2023-06-12 23:09:18 -0600

    Finished Lecture 6

--- Recursion/src/RecursiveSquares.java
+++ Recursion/src/RecursiveSquares.java
@@ -9,35 +9,35 @@ public class RecursiveSquares {
         // Draws a square centered on (x, y) of the given side length
         // with a light gray background and a black border.
         public static void drawSquare(double x, double y, double length){
-                StdDraw.show();
-                StdDraw.setPenColor(StdDraw.GRAY);
+                StdDraw.setXscale(-1.0, 2.0);
+                StdDraw.setYscale(-1.0, 2.0);
+                StdDraw.enableDoubleBuffering();
+                StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
                 StdDraw.filledSquare(x,y,length);
+                StdDraw.setPenColor(StdDraw.BLACK);
+                StdDraw.square(x,y,length);
+                StdDraw.show();
         }
 
         // Draws a recursive square pattern of order n, centered on (x, y)
         // of the given side length.
         public static void draw(int n, double x, double y, double length){
-                if (n==0){return;}
-                double half = Math.pow(0.5,n-1);
-                double halflen = half * length;
-                draw(n-1,x+half,y+half,halflen);
-                draw(n-1,x-half,y+half,halflen);
-                draw(n-1,x-half,y-half,halflen);
-                draw(n-1,x+half,y-half,halflen);
+                if (n == 0){return;}
+                double halflen = 0.5 * length;
+                draw(n-1,x-length,y+length,halflen);
+                draw(n-1,x+length,y+length,halflen);
                 drawSquare(x,y,length);
+                draw(n-1,x-length,y-length,halflen);
+                draw(n-1,x+length,y-length,halflen);
         }
     // Takes an integer command-line argument n and draws a recursive
     // square pattern of order n, centered on (0.5, 0.5) with side length 0.5.
     public static void main(String[] args) {
 
         // read integer from command line representing the number of steps
-        int n = StdIn.readInt();
+        int n = Integer.parseInt(args[0]);
         // set the scale of the coordinate system
-        StdDraw.setXscale(-1.0, 1.0);
-        StdDraw.setYscale(-1.0, 1.0);
-        StdDraw.enableDoubleBuffering();
-        draw(n,0.0,0.0,0.5);
-
+        draw(n,0.5,0.5,0.5);
         }
     
 }
